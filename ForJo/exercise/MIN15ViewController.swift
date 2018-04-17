e898b6052d15704aaa58bfe03c7779e2495d0510
賢瘦 <wilsonfur@gmail.com>
2018-04-18 07:58:17 +0800

    okay

--- ForJo/exercise/MIN15ViewController.swift
+++ ForJo/exercise/MIN15ViewController.swift
@@ -9,15 +9,21 @@
 import UIKit
 public var timer:Timer!
 var test = true
-
+var countNumber = 5
+var stepArray = ["1","DOM","2","DOM","3","DOM","4","DOM","5","DOM","6","DOM","7","DOM","8","DOM","9","DOM","10","DOM"]
+var numStepArray = ["1/10次","1/10次","2/10次","2/10次","3/10次","3/10次","4/10次","4/10次","5/10次","5/10次","6/10次","6/10次","7/10次","7/10次","8/10次","8/10次","9/10次","9/10次","10/10次","10/10次"]
+var countStep = 0
 class MIN15ViewController: UIViewController {
     
+  
+    @IBOutlet weak var keepGoingUI: UIButton!
+    @IBOutlet weak var numberStep: UILabel!
     @IBOutlet weak var btnPauseUI: UIButton!
     @IBOutlet weak var exitBTNUI: UIButton!
     @IBOutlet weak var naviBarTitle: UILabel!
     @IBOutlet weak var detail: UIView!
     func startTimer(){
-        timerB = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(tickDownB), userInfo: nil, repeats: true)
+        timerB = Timer.scheduledTimer(timeInterval: 2 ,target: self, selector: #selector(tickDownB), userInfo: nil, repeats: true)
     }
     
     func stopTimer(){
@@ -25,25 +31,36 @@ class MIN15ViewController: UIViewController {
     }
 
     @IBOutlet weak var exerciseIMG: UIView!
-    var countNumber = 5
+    @IBAction func keepGoing(_ sender: UIButton) {
+        self.startTimer()
+        self.detail.isHidden = true
+        keepGoingUI.isHidden = true
+        btnPauseUI.isHidden = false
+        exitBTNUI.isHidden = true
+    }
     
     @IBAction func btnPause(_ sender: UIButton) {
         exitBTNUI.isHidden = false
         stopTimer()
         detail.isHidden = false
         btnPauseUI.isHidden = true
+        keepGoingUI.isHidden = false
     }
-
-    @IBOutlet weak var countNumberLabel: UILabel!
-   
     @IBAction func exitBTN(_ sender: Any) {
         btnPauseUI.isHidden = false
         exitBTNUI.isHidden = true
-        self.startTimer()
-        self.detail.isHidden = true
+
     }
     
+    @IBOutlet weak var countNumberLabel: UILabel!
+   
+
     override func viewDidLoad() {
+        btnPauseUI.isHidden = true
+        keepGoingUI.isHidden = true
+        countStep = 0
+        countNumber = 5
+        numberStep.isHidden = true
         exitBTNUI.isHidden = true
         detail.isHidden = true
         exerciseIMG.isHidden = true
@@ -54,8 +71,15 @@ class MIN15ViewController: UIViewController {
     }
     
     @objc public func tickDownB(){
-        playSound(soundName:"2")
+        
+        countStep += 1
+        if countStep <= 19 {
+            numberStep.text = numStepArray[countStep]
+            playSound(soundName:stepArray[countStep])
+        } else {
+            timerB.invalidate()
         }
+    }
     @objc public func tickDown(){
         countNumber -= 1
         print(countNumber)
@@ -64,7 +88,9 @@ class MIN15ViewController: UIViewController {
             countNumberLabel.text = "\(countNumber)"
            
         } else if countNumber == 0 {
+            btnPauseUI.isHidden = false
             naviBarTitle.text = "15分鐘肌力訓練"
+            numberStep.isHidden = false
             exerciseIMG.isHidden = false
             playSound(soundName:"1")
             timer.invalidate()
@@ -72,6 +98,9 @@ class MIN15ViewController: UIViewController {
             return()
         }
 
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+          stopTimer()
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
